56a08f537392c888ad60ff6e74e3a8521b1ec5be
Adriane Kelle <100316502+adrikll@users.noreply.github.com>
2025-07-08 10:31:45 -0300

    Update classical_models.py

--- models/classical_models.py
+++ models/classical_models.py
@@ -3,7 +3,6 @@ from xgboost import XGBClassifier
 import config
 
 def get_classical_models():
-    """Retorna um dicionário de modelos clássicos não instanciados."""
     models = {
         'RandomForest': RandomForestClassifier(random_state=config.RANDOM_STATE, n_jobs=1),
         'XGBoost': XGBClassifier(random_state=config.RANDOM_STATE, eval_metric='mlogloss', n_jobs=1)
